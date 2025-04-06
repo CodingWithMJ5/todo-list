9582051263216cf47abc9d98e5d60cc8faddcbcb
CodingWithMJ <martintiangco.yt@gmail.com>
2025-04-06 22:12:26 +1200

    polish: disable button when trying to add empty task

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -2,20 +2,27 @@ import { useState } from 'react';
 
 const TodoInput = ({ handleAdd }) => {
   const [newTask, setNewTask] = useState('');
+
+  const handleSubmit = () => {
+    if (newTask.trim()) {
+      handleAdd(newTask);
+      setNewTask('');
+    }
+  };
   return (
     <div className="todo-input-container">
       <input
         type="text"
         placeholder="Go grocery shopping"
         onChange={(e) => setNewTask(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSubmit();
+          }
+        }}
         value={newTask}
       />
-      <button
-        onClick={() => {
-          handleAdd(newTask);
-          setNewTask('');
-        }}
-      >
+      <button disabled={!newTask.trim()} onClick={handleSubmit}>
         Add
       </button>
     </div>
